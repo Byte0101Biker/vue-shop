280aad217dc24ae417140ee3d36b49514536c1b1
Byte0101Biker <j.powszuk@icloud.com>
2025-12-11 21:07:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: '/vue-shop/',
   plugins: [vue()],
-})
+  base: "/vue-shop/", // 👈 obowiązkowe dla GitHub Pages
+});
