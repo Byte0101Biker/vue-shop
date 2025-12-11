5477d39699935d00cf2116f2bc8666b9fce91ed9
Byte0101Biker <j.powszuk@icloud.com>
2025-12-11 21:02:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,11 +9,10 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
-// Poczekaj aż Vue wystartuje, wtedy wybudź backend
+
 router.isReady().then(() => {
   app.mount("#app");
 
-  // 👇 WAŻNE: to wybudzi backend przy każdym wejściu na stronę
   fetch("https://vue-shop-u098.onrender.com/api/ping", { cache: "no-cache" })
     .then(() => console.log("Backend: awakened ✔️"))
     .catch(() => console.log("Backend still waking..."));
